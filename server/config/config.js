872def9f396d5015123f8287d7c1749b6feeb1e9
supercode <bee0228angel@outlook.com>
2021-12-08 14:31:56 -0500

    setup the apllo graphql express server

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clientURI: process.env.CLIENT_URL,
+  serverURI: process.env.SERVER_URL,
+  TMDURI: process.env.TMD_URL,
+  TMDKEY: process.env.TMD_KEY,
+  port: process.env.PORT,
+}
---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const express = require('express');
+
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const { port, clientURI } = require('./config/config');
+
+const app = express();
+
+const corsOptions = {
+  origin: clientURI,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+let apolloServer = null;
+async function startServer() {
+  apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: corsOptions, path: '/graphql' });
+};
+startServer();
+app.listen({ port }, () => {
+  console.log(`Apollo Server on http://localhost:${port}/graphql`);
+});
